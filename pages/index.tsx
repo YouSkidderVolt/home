4bbfe89eef43f877a86311d4b8c6b1a428427862
YouSkidderVolt <101655774+YouSkidderVolt@users.noreply.github.com>
2022-03-26 20:19:58 +0530

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -72,6 +72,7 @@ export default function Home(props: Props) {
 				I&lsquo;m currently focusing on studies 💀.
 			</Paragraph>
 			<br />
+			<GitHubSection pinnedRepos={github!} />
 			<br />
 			
 		</div>
